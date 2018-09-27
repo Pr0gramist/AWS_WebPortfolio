b376546eaa4e54b1383378d2d59356c56aa7cb93
Stivan Kitchoukov <risk@Stivans-MacBook-Pro.local>
2018-09-27 10:43:08 -0400

    changing css and header

--- src/components/footerComponent/footer.js
+++ src/components/footerComponent/footer.js
@@ -13,19 +13,19 @@ class Footer extends Component {
                 <div class="col-4"></div>
                 <div class="col-4 text-center">
                   <a target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/users/4651802/stivan">
-                    <i className="fa fa-stack-overflow fa-2x fa-fw"></i>
+                    <i id="footer_icon" className="fa fa-stack-overflow fa-2x fa-fw"></i>
                   </a>
                   <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kitchoukov/">
-                    <i className="fa fa-linkedin-square fa-2x fa-fw"></i>
+                    <i id="footer_icon" className="fa fa-linkedin-square fa-2x fa-fw"></i>
                   </a>
                   <a target="_blank" rel="noopener noreferrer" href="https://www.esu.edu">
-                    <i className="fa fa-graduation-cap fa-2x fa-fw"></i>
+                    <i id="footer_icon" className="fa fa-graduation-cap fa-2x fa-fw"></i>
                   </a>
                   <a target="_blank" rel="noopener noreferrer" href="https://plus.google.com/u/0/101176701515462868092">
-                    <i className="fa fa-google-plus-square fa-2x fa-fw"></i>
+                    <i id="footer_icon" className="fa fa-google-plus-square fa-2x fa-fw"></i>
                   </a>
                   <a target="_blank" rel="noopener noreferrer" href="https://github.com/Pr0gramist">
-                    <i className="fa fa-github-square fa-2x fa-fw"></i>
+                    <i id="footer_icon" className="fa fa-github-square fa-2x fa-fw"></i>
                   </a>
                   <br></br>
                  < p className="footer_name">{footer_text}</p>
--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -4,28 +4,50 @@ class Homepage extends Component {
     render() {
         return (
             <div className="wrap">
-                <div class="jumbotron text-center">
-                    <h1>Stivan Kitchoukov</h1>
-                    <p>Software Engineer</p>
-                    <img src={myPic} class="rounded-circle" alt="Stivan Kitchoukov" width="304" height="236"></img>
-                </div>
-                <div className="container">
+                <div id="homepage_banner" class="jumbotron text-center">
                     <div class="row align-items-center">
-                    
-                        <div class="col-sm-4">
+                        <div class="col-4">
+                            <i id="homepage_icons" class="fa fa-tachometer"></i>
+                            <h3 id="homepage_icon_text">Fast</h3>
+                            <p id="homepage_icon_text"><small>Fast load times and lag free<br></br> interaction, my highest priority.</small></p>
+                            <i id="homepage_icons" class="fa fa-lightbulb-o"></i>
+                            <h3 id="homepage_icon_text">Intuitive</h3>
+                            <p id="homepage_icon_text"><small>Strong preference for easy to <br></br>use, intuitive UX/UI.</small></p>
+                        </div>
+                        <div class="col-4">
+                            <h1 id="white_background_text">Stivan Kitchoukov</h1>
+                            <p id="white_background_text">Software Engineer</p>
+                            <img src={myPic} class="rounded-circle" alt="Stivan Kitchoukov" width="304" height="236"></img>
+                        </div>
+                        <div class="col-4">
+                            <i id="homepage_icons" class="material-icons">security</i>
+                            <h3 id="homepage_icon_text">Secure</h3>
+                            <p id="homepage_icon_text"><small>All software is created <br></br>and tested with latest security.</small></p>
+                            <i id="homepage_icons" class="fa fa-space-shuttle"></i>
+                            <h3 id="homepage_icon_text">Dynamic</h3>
+                            <p id="homepage_icon_text"><small>Websites don't have to be static, I love<br></br> making pages come to life.</small></p>
+                        </div>
+                    </div>
+                </div>
+                <div id="hompage_content">
+                    <div className="container">
+                        <div class="row align-items-center">
                         
-                            <h3>
-                                Welcome to my personal website. I'm currently working at STS, a software company in Stroudsburg, Pennsylvania. 
-                                Please take a look around!  
-                            </h3>
+                            <div class="col-sm-4">
                             
+                                <h3>
+                                    Welcome to my personal website. I'm currently working at STS, a software company in Stroudsburg, Pennsylvania. 
+                                    Please take a look around!  
+                                </h3>
+                                
+                            </div>
+                            <div class="col-sm-8">
+                                    I am passionate about building software that improves peoples everyday tasks. 
+                                    I specialize in creating financial software for clients ranging from start-ups to large enterprise corporations.
+                                    My work is produced locally from Stroudsburg, Pennsylvania. Lets say you have a software expert to your availability - What would you do?
+                            </div>
+                        
                         </div>
-                        <div class="col-sm-8">
-                                I am passionate about building software that improves peoples everyday tasks. 
-                                I specialize in creating financial software for clients ranging from start-ups to large enterprise corporations.
-                                My work is produced locally from Stroudsburg, Pennsylvania. Lets say you have a software expert to your availability - What would you do?
-                        </div>
-                    
                     </div>
                 </div>
                 
