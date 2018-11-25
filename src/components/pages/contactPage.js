9633bb6e9cc10031976b0839d249c8f20075b238
Stivan Kitchoukov <risk@Stivans-MacBook-Pro.local>
2018-11-25 12:34:58 -0500

    Formatted Contact page header

--- src/components/pages/contactPage.js
+++ src/components/pages/contactPage.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 class Contact extends Component {
     render() {
         return (
-            <h1 id="white_background_text">Contact Me</h1>
+            <div className="wrap">
+                <div id="homepage_banner" class="jumbotron text-center">
+                    <div class="row align-items-center">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4">
+                            <h1 id="white_background_text">Contact Me</h1>
+                        </div>
+                        <div class="col-md-4"></div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
