2a461d32f133671ff8d23f2704260660955a46c9
Stivan Kitchoukov <risk@Stivans-MacBook-Pro.local>
2018-12-11 09:55:52 -0500

    Added validation to contact form

--- src/components/pages/contactPage.js
+++ src/components/pages/contactPage.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
 
 class Contact extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            contact_name: '',
+            contact_email: '',
+            contact_phone: '',
+            contact_message: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event){
+        alert("Name submitted: " + this.state.contact_name);
+        alert("Email submitted: " + this.state.contact_email);
+        alert("Phone submitted: " + this.state.contact_phone);
+        alert("Message submitted: " + this.state.contact_message);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="wrap">
@@ -17,25 +48,25 @@ class Contact extends Component {
                  <div class="row align-items-center">
                     <div class="col-md-4"></div>
                     <div class="col-md-4">
-                        <from>
+                        <form onSubmit={this.handleSubmit}>
                             <div class="form-group">
                                 <label for="contact_name">Name:</label>
-                                <input type="text" class="form-control" id="contact_name" placeholder="Your Name *" name="contact_name"></input>
+                                <input type="text" value={this.state.contact_name} onChange={this.handleChange} class="form-control" id="contact_name" placeholder="Your Name *" name="contact_name" required/>
                             </div>
                             <div class="form-group">
                                 <label for="contact_email">Email:</label>
-                                <input type="email" class="form-control" id="contact_email" placeholder="Your Email *" name="contact_email"></input>
+                                <input type="email" value={this.state.contact_email} onChange={this.handleChange} class="form-control" id="contact_email" placeholder="Your Email *" name="contact_email" required/>
                             </div>
                             <div class="form-group">
                                 <label for="contact_phone">Phone:</label>
-                                <input type="text" class="form-control" id="contact_phone" placeholder="Your Phone" name="contact_phone"></input>
+                                <input type="text" value={this.state.contact_phone} onChange={this.handleChange} class="form-control" id="contact_phone" placeholder="Your Phone" name="contact_phone"/>
                             </div>
                             <div class="form-group">
                                 <label for="contact_message">Message:</label>
-                                <textarea type="text" class="form-control" id="contact_message" placeholder="Your Message *" name="contact_message"></textarea>
+                                <textarea type="text" value={this.state.contact_message} onChange={this.handleChange} class="form-control" id="contact_message" placeholder="Your Message *" name="contact_message" required/>
                             </div>
                             <button type="submit" class="btn btn-primary">Send</button>
-                        </from>
+                        </form>
                     </div>
                     <div class="col-md-4"></div>
                 </div>
