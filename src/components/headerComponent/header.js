5c24cfe35ecd571b1e81321b0c8a573660efa96d
Stivan Kitchoukov <risk@Stivans-MacBook-Pro.local>
2018-12-19 16:26:41 -0500

    Removed Link 1 and Link 2

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -8,12 +8,15 @@ class Header extends Component {
             <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
                 <Link class="navbar-brand" to="/">STIVAN KITCHOUKOV</Link>
                 <ul class="navbar-nav">
+
+                    {/*
                     <li class="nav-item">
                         <a class="nav-link" href="#">Link 1</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Link 2</a>
                     </li>
+                    */}
                     <li class="nav-item">
                         <Link class="nav-link" to="/Contact">Contact</Link>
                     </li>
